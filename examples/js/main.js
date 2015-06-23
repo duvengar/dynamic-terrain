f2ffc4599614d46caff74677de2ecd76baa20e45
Julien Vergnaud <pixeluser@Phoenix.local>
2015-06-23 18:49:09 -0300

    Add new terrain shader material.

--- examples/js/main.js
+++ examples/js/main.js
@@ -18,13 +18,13 @@ var controls;
 
 var sceneRenderTarget;
 
-var uniformsNoise, uniformsNormal, uniformsTerrain, heightMap, normalMap, quadTarget;
+var uniformsNoise, uniformsNormal, uniformsTerrain, uniformsTerrainbis, heightMap, normalMap, quadTarget;
 
 var directionalLight, pointLight, pointLight2;
 
 var composer;
 
-var terrain;
+var terrain, terrainbis;
 
 var textureCounter = 0;
 
@@ -40,6 +40,8 @@ var animateTerrain = false;
 
 var textMesh1;
 
+var fogColor = new THREE.Color("rgb(1,1,1)");
+
 var mlib = {};
 
 init();
@@ -49,46 +51,68 @@ function init() {
 
   container = document.getElementById('container');
 
+
+
+
+
   // SCENE (RENDER TARGET)
 
   sceneRenderTarget = new THREE.Scene();
   cameraOrtho = new THREE.OrthographicCamera( SCREEN_WIDTH / - 2, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, SCREEN_HEIGHT / -2 , -10000, 10000 );
-  cameraOrtho.position.set(0, 0 , -100);
   sceneRenderTarget.add(cameraOrtho);
+
+
+
+
 
   // CAMERA
 
-  camera = new THREE.PerspectiveCamera(40, SCREEN_WIDTH / SCREEN_HEIGHT, 20, 10000);
+  camera = new THREE.PerspectiveCamera(50, SCREEN_WIDTH / SCREEN_HEIGHT, 20, 10000);
   camera.position.set(-1200, 200 , -700);
   camera.rotation.set(-3, -1, -3);
+
+
+
+
 
   // CONTROLS
 
   controls = new THREE.OrbitControls(camera);
-  controls.rotateSpeed = .2;
-  controls.zoomSpeed = .2;
+  controls.rotateSpeed = .1;
+  controls.zoomSpeed = .1;
+
+
+
+
 
   // SCENE (FINAL)
 
   scene = new THREE.Scene();
 
-  scene.fog = new THREE.Fog(0x000000, 450, 3200);
+  scene.fog = new THREE.Fog(0x000000, 350, 2500);
+
+
+
+
 
   // LIGHTS
 
-  scene.add(new THREE.AmbientLight(0xffffff));
+  scene.add(new THREE.AmbientLight(0xffffff), 1.5);
 
-  directionalLight = new THREE.DirectionalLight(0xffffff, 1.15);
+  // directionalLight = new THREE.DirectionalLight(0x00ff99, 3);
 
-  directionalLight.position.set(500, 2000, 0);
+  // directionalLight.position.set(1000, 1000, 20);
 
-  scene.add(directionalLight);
+  // scene.add(directionalLight);
+
+
+
 
   // HEIGHT + NORMAL MAPS
 
   var normalShader = THREE.NormalMapShader;
 
-  var rx = 128, ry = 128;
+  var rx = 200, ry = 200;
 
   var pars = {minFilter: THREE.LinearMipmapLinearFilter, magFilter: THREE.LinearFilter, format: THREE.RGBAFormat};
 
@@ -97,45 +121,48 @@ function init() {
   heightMap.generateMipmaps = false;
 
   normalMap = new THREE.WebGLRenderTarget(rx, ry, pars);
+
   normalMap.generateMipmaps = false;
 
   uniformsNoise = {
 
-    time:   {type: 'f', value: 1.0},
+    time:   {type: 'f', value: 0.1},
     scale:  {type: 'v2', value: new THREE.Vector2(2.5, 2.5)},
     offset: {type: 'v2', value: new THREE.Vector2(0, 0)}
 
   };
 
   uniformsNormal = THREE.UniformsUtils.clone(normalShader.uniforms);
-
   uniformsNormal.height.value = 0.5;
   uniformsNormal.resolution.value.set(rx, ry);
   uniformsNormal.heightMap.value = heightMap;
 
   var vertexShader = document.getElementById('vertexShader').textContent;
 
+
+
+
   // TERRAIN SHADER
 
   var terrainShader = THREE.ShaderTerrain['terrain'];
 
   uniformsTerrain = THREE.UniformsUtils.clone(terrainShader.uniforms);
-
-  uniformsTerrain['tNormal'].value = normalMap;
-
-  uniformsTerrain['uNormalScale'].value = 0.5;
-
   uniformsTerrain['tDisplacement'].value = heightMap;
-
   uniformsTerrain['diffuse'].value.setHex (0xffffff);
+  uniformsTerrain['uDisplacementScale'].value = -600;
 
-  uniformsTerrain['uDisplacementScale'].value = 500;
+  uniformsTerrainbis = THREE.UniformsUtils.clone(terrainShader.uniforms);
+  uniformsTerrainbis['tDisplacement'].value = heightMap;
+  uniformsTerrainbis['diffuse'].value= fogColor;
+  uniformsTerrainbis['uDisplacementScale'].value = -600;
 
   var params = [
 
       ['heightmap',  document.getElementById('fragmentShaderNoise').textContent,   vertexShader, uniformsNoise, false],
       ['normal',     normalShader.fragmentShader,  normalShader.vertexShader, uniformsNormal, false],
-      ['terrain',    terrainShader.fragmentShader, terrainShader.vertexShader, uniformsTerrain, true]
+      ['terrain',    terrainShader.fragmentShader, terrainShader.vertexShader, uniformsTerrain, true],
+      ['terrainbis',    terrainShader.fragmentShader, terrainShader.vertexShader, uniformsTerrainbis, true],
+
    ];
 
   var material;
@@ -149,6 +176,7 @@ function init() {
       fragmentShader: params[ i ][ 1 ],
       lights:         params[ i ][ 4 ],
       fog:            true
+
     });
 
     mlib[ params[ i ][ 0 ] ] = material;
@@ -156,6 +184,7 @@ function init() {
   }
 
   var plane = new THREE.PlaneBufferGeometry(SCREEN_WIDTH, SCREEN_HEIGHT);
+
 
   quadTarget = new THREE.Mesh(plane, new THREE.MeshBasicMaterial({color: 0x000000}));
 
@@ -168,17 +197,26 @@ function init() {
   var geometryTerrain = new THREE.PlaneBufferGeometry(4000, 4000, rx, ry);
 
   geometryTerrain.computeTangents();
+  geometryTerrain.computeVertexNormals();
 
   mlib['terrain'].wireframe = true;
 
+
+
   terrain = new THREE.Mesh(geometryTerrain, mlib['terrain']);
+  terrainbis = new THREE.Mesh(geometryTerrain, mlib['terrainbis'] );
 
   terrain.position.set(0, 100, 0);
   terrain.rotation.x = -Math.PI / 2 ;
-  terrain.rotation.y =  Math.PI ;
+  //terrain.rotation.y =  Math.PI ;
+
+  terrainbis.position.set(0, 98, 0);
+  terrainbis.rotation.x = -Math.PI / 2 ;
+  //terrainbis.rotation.y =  Math.PI ;
 
 
   scene.add(terrain);
+  scene.add(terrainbis);
 
   // RENDERER
 
@@ -238,7 +276,12 @@ function render() {
     lightVal = THREE.Math.clamp(lightVal + 0.5 * delta * lightDir, fLow, fHigh);
     var valNorm = (lightVal - fLow) / (fHigh - fLow);
 
+    //scene.fog.color = new THREE.Color("rgb(30,30,30)");
     scene.fog.color = new THREE.Color("rgb(30,30,30)");
+
+    // console.log( camera.position);
+    // console.log( camera.rotation);
+
 
     renderer.setClearColor( scene.fog.color, 1);
 
@@ -250,7 +293,7 @@ function render() {
 
       uniformsNoise['time'].value += delta * animDelta;
 
-      uniformsNoise['offset'].value.x -= delta * 0.1;
+      uniformsNoise['offset'].value.x += delta * 0.01;
 
       quadTarget.material = mlib['heightmap'];
 
